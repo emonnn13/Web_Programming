313306636f858728be9bd03d37aa5be5ff94b219
emonnn13 <emonhasan29@gmail.com>
2021-08-29 18:58:43 +0600

    Minor changes.

--- react_test/invertory/inventory/src/App.js
+++ react_test/invertory/inventory/src/App.js
@@ -1,26 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <p>EMON IS HERE</p>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Info />
+      <AddItem />
     </div>
   );
+}
+function Info(){
+  const title = "This is a title variable."
+  let showTime = false
+  return (
+   <div>
+      {showTime ? title : "Show time was false."}
+     <h1>This is the info page</h1>
+     <p> Mange your stuff.</p>
+   </div>
+  )
+}
+
+function AddItem(){
+  return (
+    <form>
+      <label for="text-form"> Type Something: </label>
+      <input type="text" id="text-form" />
+    </form>
+  )
 }
 
 export default App;
